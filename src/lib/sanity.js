e5aa7667f1676c8a48707f0d46f6aa7dcbb9183b
jacobdcl <98243210+jacobdcl@users.noreply.github.com>
2025-01-22 21:41:11 -0500

    Fix Sanity client configuration

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -5,9 +5,9 @@ export const client = createClient({
   projectId: 'tuca5i9i',
   dataset: 'production',
   apiVersion: '2024-01-01',
-  useCdn: true,
+  useCdn: false,
   token: import.meta.env.VITE_SANITY_TOKEN,
-  withCredentials: true,
+  withCredentials: false,
   cors: true,
   ignoreBrowserTokenWarning: true
 });
